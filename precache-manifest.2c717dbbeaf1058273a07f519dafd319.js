4f5a4d46b76bc81219f8cb282bed53fcae07b771
matei.eduard <eduardmatei0@yahoo.com>
2019-08-22 16:03:43 +0300

    Updates

---
+++ precache-manifest.2c717dbbeaf1058273a07f519dafd319.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d64a90b0b0145dd1d93f2efaadfa1f",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "215b4cc150eef1c43064",
+    "url": "/Monsters-Rolodex/static/css/main.352ac8dd.chunk.css"
+  },
+  {
+    "revision": "01d8317fe3a3835ac7c6",
+    "url": "/Monsters-Rolodex/static/js/2.f4bca13b.chunk.js"
+  },
+  {
+    "revision": "215b4cc150eef1c43064",
+    "url": "/Monsters-Rolodex/static/js/main.1adbeefb.chunk.js"
+  },
+  {
+    "revision": "b20344563c251952afc8",
+    "url": "/Monsters-Rolodex/static/js/runtime~main.447c9861.js"
+  }
+]);
